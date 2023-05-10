bf2e5dce1cdb7917ecfd0066cd09a24345ada9ac
Natalia Horbunova <nata.horbunova@gmail.com>
2023-05-11 02:11:55 +0300

    data.js refactored

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './Components/App';
-import {data} from "./data/constants";
+import {data} from "./data/data";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
